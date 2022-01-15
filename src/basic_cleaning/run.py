675d12ff7ce0bc475cdb90821ae0074579ca371a
rafalghul <64754323+rafalghul@users.noreply.github.com>
2022-01-14 21:19:14 -0500

    Added geospacial bug fix

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -39,6 +39,10 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
     logger.info('Successfully converted last_review to datetime')
 
+    # Fig geolocation bug
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save results to CSV
     df.to_csv("clean_sample.csv", index=False)
 
